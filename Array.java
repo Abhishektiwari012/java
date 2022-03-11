200748616a2972a659618e75ac84e7a6de72801f
Abhishektiwari012 <101421980+Abhishektiwari012@users.noreply.github.com>
2022-03-12 00:33:25 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+public final class Array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[1]=9;
+        marks[2]=6;
+        marks[3]=2;
+        System.out.println(marks[0]);
+
+    }
+}
